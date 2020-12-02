4e36cc6c2e38d22263773201de9df87bd49164de
o.s.i <o.s.i.hts@gmail.com>
2020-12-02 19:31:23 +0200

    objects all

--- js/objects.js
+++ js/objects.js
@@ -11,3 +11,13 @@ var scores = {
 scores["Вася"] += 5;
 
 console.log(scores);
+
+// exercise 2
+
+var myCrazyObject = {
+    "name": "Нелепый объект",
+    "some array": [7, 9, { purpose: "путаница", number: 123 }, 3.3],
+    "random animal": "Банановая акула"
+};
+
+console.log(myCrazyObject ["some array"][2].number);
