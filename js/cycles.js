0fc1908baaaf7109b97bce167c09769df08d52af
o.s.i <o.s.i.hts@gmail.com>
2020-12-03 13:50:00 +0200

    exercise if else

--- js/cycles.js
+++ js/cycles.js
@@ -1,2 +1,12 @@
 document.body.innerHTML = "learn Js cycles"
 
+var name = "";
+if (name === "Вадим") {
+    console.log("Привет мне");
+}   else if (name === "Владимир") {
+    console.log("Привет, папа");
+}   else if (name === "Любовь") {
+    console.log("Привет, мама");
+}   else {
+    console.log("Привет, незнакомец");
+}
